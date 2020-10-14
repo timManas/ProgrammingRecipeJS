dfc582f8153dd51216fc88e46424834d60c12aed
TimManas <timothy.romero.manas@gmail.com>
2020-10-14 19:32:35 -0400

    Added Arrow functions Recipe

---
+++ src/Fundamentals/ArrowFunctions/arrowFunctions.js
@@ -0,0 +1,12 @@
+// Single Line Arrow Funcitons
+console.log("Single Line Arrow Functions")
+let sum = (a,b) => a+b
+console.log(sum(2,3))
+
+// MultiLine Arrow Functions
+console.log("Multiline Arrow FUnctions")
+let multiply = (a,b) => {
+    console.log("Inside MultiLine FUnciton")
+    return a * b
+}
+console.log(multiply(4,5))
