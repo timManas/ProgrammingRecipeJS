ec9feefbab131fba967df31c1301ba1a61570d24
Tim M <timothy.romero.manas@gmail.com>
2022-06-06 22:07:22 -0400

    Finished Array methods

--- src/DataStructures/Arrays/arrays.js
+++ src/DataStructures/Arrays/arrays.js
@@ -221,7 +221,48 @@ const arrJoin1 = [1, 2, 3, 4, 5, 6, 7, 8]
 console.log('joined: ' + arrJoin1.join(''))
 console.log('joined: ' + arrJoin1.join(' '))
 
-// reduce/reduceRight
+// reduce/re""duceRight
+console.log('\nReduceRight')
+//syntax: arr.reduce(function(accumulator, item, index, array) {...}, [initial])
+// What ? Used to calculate a single value based on the array
+// accumulator: is the result of the previous funciton call, equals iinitial the first time  (if provided)
+// item: is the current array item
+// index: is the position
+// array: is the array
+// How ? As the function is applied, the previous function call is passed to the next one as the first argument
+const arrReduce = [1, 2, 3, 4, 5]
+let result = arrReduce.reduce((sum, current) => sum + current, 0)
+console.log('reduce result: ' + result)
 
-// fill
-// copy
+/**
+ Summary
+To add/remove elements:
+
+push(...items) – adds items to the end,
+pop() – extracts an item from the end,
+shift() – extracts an item from the beginning,
+unshift(...items) – adds items to the beginning.
+splice(pos, deleteCount, ...items) – at index pos deletes deleteCount elements and inserts items.
+slice(start, end) – creates a new array, copies elements from index start till end (not inclusive) into it.
+concat(...items) – returns a new array: copies all members of the current one and adds items to it. If any of items is an array, then its elements are taken.
+To search among elements:
+
+indexOf/lastIndexOf(item, pos) – look for item starting from position pos, return the index or -1 if not found.
+includes(value) – returns true if the array has value, otherwise false.
+find/filter(func) – filter elements through the function, return first/all values that make it return true.
+findIndex is like find, but returns the index instead of a value.
+To iterate over elements:
+
+forEach(func) – calls func for every element, does not return anything.
+To transform the array:
+
+map(func) – creates a new array from results of calling func for every element.
+sort(func) – sorts the array in-place, then returns it.
+reverse() – reverses the array in-place, then returns it.
+split/join – convert a string to array and back.
+reduce/reduceRight(func, initial) – calculate a single value over the array by calling func for each element and passing an intermediate result between the calls.
+Additionally:
+
+Array.isArray(arr) checks arr for being an array.
+
+ */
