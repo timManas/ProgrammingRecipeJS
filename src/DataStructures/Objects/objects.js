48b0a682114dee3b0e64c4aca62fe8d63824f4fc
Tim M <timothy.romero.manas@gmail.com>
2022-07-04 22:23:32 -0400

    Adding object transformation

--- src/DataStructures/Objects/objects.js
+++ src/DataStructures/Objects/objects.js
@@ -17,5 +17,20 @@ console.log('Objects.entries() - ' + Object.entries(user1))
 /*
 Note
 - Object.* methods return "real" array objects and NOT just an iterable
+ */
 
+console.log('\nTransform an Object')
+let prices = { banana: 1, orange: 2, meat: 4 }
+
+let doublePrices = Object.fromEntries(
+  Object.entries(prices).map((entry) => [entry[0], entry[1] * 2])
+)
+console.log('Meat prices:' + doublePrices.meat)
+
+/**
+ * So what happened ?
+ * 1. Use Object.entries() to get the key/value pair from prices
+ * 2. Use array method map to transform key/value pair to double prices
+ * 3. Use Object.fromEntries() on resulting array to convert back to Object
+ *
  */
