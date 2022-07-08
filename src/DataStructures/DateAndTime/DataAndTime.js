c7d2294fdf5f032e7f1d96255a65c1d4506c4a31
Tim M <timothy.romero.manas@gmail.com>
2022-07-07 23:17:43 -0400

    Adding Date set and misc date getters

--- src/DataStructures/DateAndTime/DataAndTime.js
+++ src/DataStructures/DateAndTime/DataAndTime.js
@@ -27,3 +27,35 @@ console.log('Date2: ' + date2)
 
 let date3 = new Date(2011, 0, 1)
 console.log('Date3: ' + date3)
+
+// Get Miscellaneous
+console.log('getHours: ' + now.getHours())
+console.log('getDay: ' + now.getDay())
+console.log('getFullYear: ' + now.getFullYear())
+console.log('getMilliseconds: ' + now.getMilliseconds())
+console.log('getMinutes: ' + now.getMinutes())
+console.log('getMonth: ' + now.getMonth())
+console.log('getTime: ' + now.getTime())
+console.log('getTimezoneOffset: ' + now.getTimezoneOffset())
+console.log('getUTCDate: ' + now.getUTCDate())
+console.log('getUTCDay: ' + now.getUTCDay())
+console.log('getUTCSeconds: ' + now.getUTCSeconds())
+
+// Setting Time
+console.log('\nSetting Time')
+let today = new Date()
+today.setHours(0)
+console.log('today set Hours: ' + today)
+
+today.setHours(0, 0, 0, 0)
+console.log(
+  'today setHours(0, 0, 0, 0): ' + today + '  Note: Time is set to 00:00:00'
+)
+
+// AutoCorrection
+// Handy feature allows us to set the proper date incase we fat finger the time and date
+console.log('\nAutoCorrection')
+let date4 = new Date(2022, 0, 32)
+console.log(
+  'Date: ' + date4 + '    note: Notice the date is set to Feb 1st instead'
+)
