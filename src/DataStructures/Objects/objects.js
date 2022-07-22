7a54579aec40c1a9eeb8e4d99bfd61dea211e87e
Tim M <timothy.romero.manas@gmail.com>
2022-07-21 23:47:28 -0400

    Adding property flag recipe

--- src/DataStructures/Objects/objects.js
+++ src/DataStructures/Objects/objects.js
@@ -34,3 +34,29 @@ console.log('Meat prices:' + doublePrices.meat)
  * 3. Use Object.fromEntries() on resulting array to convert back to Object
  *
  */
+
+console.log('\nProperty Flags and descriptors')
+/**
+ * Syntax: let descriptor = Object.getOwnPropertyDescriptors(obj, propertyName)
+ * Syntax for changing property = Object.defineProperty(obj, propertyName, descriptor)
+ *
+ * Besides key/value pair, objects can have three special attributes
+ * writeables - If true, value can be changed, otherwise its read-only
+ * enumerable - If true, then listed in loops, otherwise not listed
+ * configuarable - if true, property can be deleted/modified, otherwise not
+ *
+ * Generally - They are all true when we create an object but can be changed at any time
+ */
+
+// Ex1
+let user2 = { name: 'John' }
+let descriptor = Object.getOwnPropertyDescriptors(user2, 'name')
+console.log(JSON.stringify(descriptor, null, 2)) // you will see the properties object user2
+
+// To change the flag we use Object.defineProperty
+// NOTE: If flag is supplied, it is assumed false
+// Ex2:
+let user3 = {}
+Object.defineProperty(user3, 'name', { value: 'John' })
+let descriptor3 = Object.getOwnPropertyDescriptors(user3, 'name')
+console.log(JSON.stringify(descriptor3, null, 2))
