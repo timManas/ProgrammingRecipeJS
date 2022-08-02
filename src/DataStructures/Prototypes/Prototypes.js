ba5f06df4e8a525aafe63f200c728ba66d0a7b75
Tim M <timothy.romero.manas@gmail.com>
2022-08-01 21:15:11 -0400

    Added more prototype recipes

--- src/DataStructures/Prototypes/Prototypes.js
+++ src/DataStructures/Prototypes/Prototypes.js
@@ -64,3 +64,23 @@ console.log(admin.fullName)
 admin.fullName = 'Alice Cooper'
 console.log(admin.fullName) // Alice Cooper
 console.log(user.fullName) // John Smith, state of the user is protected
+
+// Note: For ... loop also iterates over inherited properties too
+console.log('\nFor... loop also iteraves over inherited properties too ')
+let animal4 = { eats: true }
+let rabbit = { jumps: true, __proto__: animal4 }
+for (let prop in rabbit) console.log('Property: ' + prop)
+
+/**
+ 
+Summary
+ - In JavaScript, all objects have a hidden [[Prototype]] property that’s either another object or null.
+ - We can use obj.__proto__ to access it (a historical getter/setter, there are other ways, to be covered soon).
+ - The object referenced by [[Prototype]] is called a “prototype”.
+ - If we want to read a property of obj or call a method, and it doesn’t exist, then JavaScript tries to find it in the prototype.
+ - Write/delete operations act directly on the object, they don’t use the prototype (assuming it’s a data property, not a setter).
+ - If we call obj.method(), and the method is taken from the prototype, this still references obj. So methods always work with the current object even if they are inherited.
+ - The for..in loop iterates over both its own and its inherited properties. All other key/value-getting methods only operate on the object itself.
+
+
+ */
