736e72ee6ca89b0deb4fc8d4749884f027ba2bc8
Tim M <timothy.romero.manas@gmail.com>
2022-08-07 20:48:38 -0400

    Adding Class expression recipe

--- src/DataStructures/Classes/Class.js
+++ src/DataStructures/Classes/Class.js
@@ -22,3 +22,33 @@ class User {
 let user1 = new User('John')
 console.log('name: ' + user1.name)
 console.log('say:' + user1.sayHi())
+
+// Class Expression
+console.log('\nClass Expression')
+// What ? Classes can be defined inside another expression, passed around, returned, assigned, etc
+
+// Ex1 - Class expression
+let User1 = class {
+  sayHi() {
+    console.log('Hello')
+  }
+}
+
+// Ex2 - Class expression can have names
+let User2 = class MyClass {
+  sayHi() {
+    console.log('Hi')
+  }
+}
+new User2().sayHi()
+
+// Ex3 - Class defined on demand
+function makeClass(phrase) {
+  return class {
+    sayHi() {
+      console.log('Yo')
+    }
+  }
+}
+let User3 = makeClass('hihihihi')
+new User3().sayHi()
