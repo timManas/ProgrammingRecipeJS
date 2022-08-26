a2dc77d8dea381ea7afa1abd470454d07c8f9857
Tim M <timothy.romero.manas@gmail.com>
2022-08-26 00:20:41 -0400

    Adding then recipe

--- src/DataStructures/Promises/promises.js
+++ src/DataStructures/Promises/promises.js
@@ -9,17 +9,50 @@ console.log('\nPromise')
 
 // Promises object return new Promise constructor has these internal properties
 // Promises contains state which initially is "pending" then moves to either resolved or reject
+// Promises return another promise ...those can also resolve or reject. Hence promises can be chained together
 
 // Ex1: Resolved
 let promise = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    resolve('done')
-  }, 1000)
+  setTimeout(() => resolve('done'), 1000)
 })
 
-// Ex2: Reject
-let rejected = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    reject(new Error('Whoops'))
-  }, 1000)
+// Ex2: Reject. Commenting this out. Will throw and error
+// let promise1 = new Promise(function (resolve, reject) {
+//   // after 1 second signal that the job is finished with an error
+//   setTimeout(() => reject(new Error('Whoops!')), 1000)
+// })
+
+// Then, catch
+// syntax is: promise.then(function(result) {}, function(error) P{} )
+// 1st argument of .then is run when the promise is resolved and receives the result
+// 2nd argument of .then is a function when the promise is rejected and receives an error
+console.log('\nthen')
+
+// Ex1: Resolve
+let promise2 = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve('done!'), 1000)
 })
+
+// resolve runs the first function in .then
+promise2.then(
+  (result) => console.log(result), // shows "done!" after 1 second
+  (error) => console(error) // doesn't run
+)
+
+// Ex2: Rejected. Comment this out. Will throw error
+// let promise3 = new Promise(function (resolve, reject) {
+//   setTimeout(() => reject('error!'), 1000)
+// })
+
+// // resolve runs the first function in .then
+// promise2.then(
+//   (result) => console.log(result), // shows "done!" after 1 second
+//   (error) => console(error) // shows error
+// )
+
+// Ex3: More streamlined solution
+let promise4 = new Promise((resolve) => {
+  setTimeout(() => resolve('Doneeeee'), 1000)
+})
+
+promise4.then(console.log('Hello'))
