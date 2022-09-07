8ca605b71328307b7c648a38445358198f2e0230
Tim M <timothy.romero.manas@gmail.com>
2022-09-06 22:11:18 -0400

    Adding descirption foer Await/Async recipe

--- src/DataStructures/Promises/AsyncAwait.js
+++ src/DataStructures/Promises/AsyncAwait.js
@@ -1,6 +1,10 @@
 // Async keyword
 // Placed on a function.
 // Purpose is convert function to return a promise
+
+const { rejects } = require('assert')
+const { resolve } = require('path')
+
 // Also allows 'await' to work inside async functions
 console.log('Async')
 
@@ -11,3 +15,19 @@ async function f() {
 
 let f1 = f()
 f1.then((value) => console.log(value))
+
+// Await Keyword
+// Makes JS wait until the promise settles and returns its result
+console.log('\nAwait Keyword')
+
+// Ex1
+async function awaitFuncton() {
+  let promise = Promise.resolve('Resolved')
+
+  let result = await promise
+  console.log('Waiting for result: ' + result) // Notice this waited for the Promise to get settled ...
+  console.log('Finished running Function')
+}
+
+awaitFuncton()
+console.log('Ex2 Done') // Notice this will get printed first before the awaitFunction contents. It is due to the current stack finishing first then the Promise Stack ...
