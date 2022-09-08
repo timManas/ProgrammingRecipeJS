75bff4d4d83caaa00eb2ddacabef85d258247f08
Tim M <timothy.romero.manas@gmail.com>
2022-09-07 23:22:59 -0400

    Adding more examples for generators

--- src/DataStructures/Generators/Generators.js
+++ src/DataStructures/Generators/Generators.js
@@ -2,7 +2,10 @@
 // Can only return one value but also return multiple using yield
 // Similar to arrays or Queue
 // Keeps track of the current value iterated
-// Returns done:true ocne we reach the very bottomn
+// Returns done:true ocne we reach the very bottom
+
+// Note: Generators are iterable
+
 console.log('Generators Functions')
 
 // Ex1
@@ -21,3 +24,9 @@ console.log('value: ' + JSON.stringify(value)) // value: {"value":2,"done":false
 
 value = generator.next()
 console.log('value: ' + JSON.stringify(value)) // value: {"value":3,"done":true}
+
+// Ex2: Generators are iterable
+let generator2 = generateSequence()
+for (let value of generator2) {
+  console.log('value: ' + value) // Notice this only shoqws 1,2 ...Not 3. Why ? Because for loop ignores the last value. So if we want everything, we use the .next() approach
+}
