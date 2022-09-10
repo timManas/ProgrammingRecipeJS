308069e9c399254f340ed82f6398e0c8ec942897
Tim M <timothy.romero.manas@gmail.com>
2022-09-10 11:17:52 -0400

    Adding multi export module

--- src/DataStructures/Modules/Module.js
+++ src/DataStructures/Modules/Module.js
@@ -3,7 +3,12 @@
 // Modules can load each other and use special directives 'export' and 'import' to interchange functionality
 console.log('Modules')
 
-// Export files in sayHi.js...then import the file using the import function
-import { sayHi } from './sayHi.js'
-console.log(sayHi)
-sayHi('HELLO')
+//Ex: Single Export -  Export files in sayHi.js...then import the file using the import function
+import { sayHi } from './singleExport.js'
+console.log('importing: ' + sayHi)
+sayHi('HIIIII')
+
+// Ex2: MultiExport
+import { sayWow, sayHello } from './multiExport.js'
+sayWow('WOOOW')
+sayHello('HELLLLOOO')
---
+++ src/DataStructures/Modules/multiExport.js
@@ -0,0 +1,9 @@
+function sayWow(value) {
+  console.log('sayWow: ' + value)
+}
+
+function sayHello(value) {
+  console.log('sayHello: ' + value)
+}
+
+export { sayWow, sayHello }
