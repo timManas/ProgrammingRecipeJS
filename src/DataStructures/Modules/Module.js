bc6dc1444ec860961f07eae854e170e1232b6727
Tim M <timothy.romero.manas@gmail.com>
2022-09-12 22:57:00 -0400

    Adding description for modules

--- src/DataStructures/Modules/Module.js
+++ src/DataStructures/Modules/Module.js
@@ -12,3 +12,20 @@ sayHi('HIIIII')
 import { sayWow, sayHello } from './multiExport.js'
 sayWow('WOOOW')
 sayHello('HELLLLOOO')
+
+/**
+Summary
+
+To summarize, the core concepts are:
+
+1. A module is a file. To make import/export work, browsers need <script type="module">. Modules have several differences:
+- Deferred by default.
+- Async works on inline scripts.
+
+2. To load external scripts from another origin (domain/protocol/port), CORS headers are needed.
+3. Duplicate external scripts are ignored.
+4. Modules have their own, local top-level scope and interchange functionality via import/export.
+5. Modules always use strict.
+    - Module code is executed only once. Exports are created once and shared between importers.
+
+ */
