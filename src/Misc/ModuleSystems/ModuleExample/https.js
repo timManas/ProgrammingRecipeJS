a51f3bf3c4e5df0efe03e11ea633aac50e3f0d23
Tim M <timothy.romero.manas@gmail.com>
2023-11-14 20:20:53 -0500

    Refactor code base

--- src/Misc/ModuleSystems/ModuleExample/https.js
+++ src/Misc/ModuleSystems/ModuleExample/https.js
@@ -4,8 +4,8 @@
  */
 console.log('Example WITHOUT Destructuring')
 
-const request = require('./request.cjs')
-const response = require('./response.cjs')
+const request = require('./request.js')
+const response = require('./response.js')
 
 function makeRequest(url, data) {
   console.log('timeout: ' + request.TIMEOUT_SEC)
@@ -19,8 +19,8 @@ console.log(makeRequest('https://www.google.com', 'hello') + '\n\n\n')
  Notice we can access the function names directly instead of using the '.' notation
  */
 console.log('Example using Destructuring')
-const { sendReq, TIMEOUT_SEC } = require('./request.cjs')
-const { read } = require('./response.cjs')
+const { sendReq, TIMEOUT_SEC } = require('./request.js')
+const { read } = require('./response.js')
 
 function makeRequestDestructuring(url, data) {
   console.log('timeout: ' + TIMEOUT_SEC)
