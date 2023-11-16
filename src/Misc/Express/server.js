9eac014a8f2903d6d9653655289f283ce782996f
Tim M <timothy.romero.manas@gmail.com>
2023-11-15 22:05:58 -0500

    Adding parameterized routing

--- src/Misc/Express/server.js
+++ src/Misc/Express/server.js
@@ -1,12 +1,44 @@
 import express from 'express'
 
 const app = express()
-const PORT = 5001
+const PORT = 5000
 
-// This is a next() example
+let enableNext = false
+
+const friends = [
+  {
+    id: 0,
+    name: 'Tim',
+  },
+  {
+    id: 1,
+    name: 'John',
+  },
+]
+
+app.get('/friends', (req, res, next) => {
+  res.json(friends)
+})
+
+// Parameterized Route
+app.get('/friends/:friendId', (req, res, next) => {
+  const friendId = Number(req.params.friendId)
+  const friend = friends[friendId]
+  if (friend) {
+    res.status(200).json(friend)
+  } else {
+    res.status(404).json({
+      error: `friend does not exists: ${friendId}`,
+    })
+  }
+})
+
 app.get('/', (req, res, next) => {
+  if (enableNext) {
+    next()
+  }
+
   res.send('Hello World !')
-  next()
 })
 
 app.get('/', (req, res, next) => {
