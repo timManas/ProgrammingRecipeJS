09db07bd21e14a1c87c65df79bb1e74ca33643a7
Tim M <timothy.romero.manas@gmail.com>
2023-11-18 10:49:25 -0500

    Adding Jest Testcase example

---
+++ src/Misc/Express/routes/friendsrouter.test.js
@@ -0,0 +1,13 @@
+describe('Test GET /TEST1', () => {
+  test('Should response with 200 success', () => {
+    const response = 200
+    expect(response).toBe(200)
+  })
+})
+
+describe('Test POST /TEST2', () => {
+  test('Should response with 200 success', () => {
+    const response = 200
+    expect(response).toBe(200)
+  })
+})
