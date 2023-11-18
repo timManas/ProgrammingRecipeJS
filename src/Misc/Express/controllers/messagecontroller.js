c7a22ccddeb0ea79e9a9bd79a89c93046ac8882d
Tim M <timothy.romero.manas@gmail.com>
2023-11-18 00:43:53 -0500

    Send File Example

--- src/Misc/Express/controllers/messagecontroller.js
+++ src/Misc/Express/controllers/messagecontroller.js
@@ -1,7 +1,21 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 function getMessages(req, res, next) {
   res.json({
     message: 'This is a message',
   })
+}
+
+function getMotivation(req, res, next) {
+  // 1st get the __filename and then __dirname
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+
+  // path.join() will join all the names under a single path
+  const fullPath = path.join(__dirname, '..', 'public', 'Yesterday.png')
+  console.log('fullPath: ' + fullPath)
+  res.sendFile(fullPath)
 }
 
 function sendBadMessage(req, res, next) {
@@ -10,4 +24,4 @@ function sendBadMessage(req, res, next) {
   })
 }
 
-export { getMessages, sendBadMessage }
+export { getMessages, sendBadMessage, getMotivation }
--- src/Misc/Express/routes/messagesrouter.js
+++ src/Misc/Express/routes/messagesrouter.js
@@ -3,5 +3,6 @@ import * as msgController from '../controllers/messagecontroller.js'
 
 const messagesRouter = express.Router()
 messagesRouter.get('/', msgController.getMessages)
+messagesRouter.get('/motivation', msgController.getMotivation)
 
 export { messagesRouter }
