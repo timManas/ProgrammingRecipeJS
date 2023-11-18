1241207507f8375bef897a43ecd476de09dbcf52
Tim M <timothy.romero.manas@gmail.com>
2023-11-18 11:57:33 -0500

    Finished GET Jest testcase

--- src/Misc/Express/routes/friendsrouter.test.js
+++ src/Misc/Express/routes/friendsrouter.test.js
@@ -1,11 +1,24 @@
-describe('Test GET /TEST1', () => {
-  test('Should response with 200 success', () => {
-    const response = 200
-    expect(response).toBe(200)
+// const request = require('supertest')
+// const app = require('../app.js')
+
+import request from 'supertest'
+import { app } from '../app.js'
+
+/**
+  Why is request set to supertest ?
+  - Because it will supertest will be the tool which makes the request on the API
+  
+  You need to create an app.js file here in order for supertest to test the API
+*/
+
+describe('Test GET /friends', () => {
+  test('Should response with 200 success', async () => {
+    const response = await request(app).get('/friends').expect(200)
+    console.log('response: ' + JSON.stringify(response))
   })
 })
 
-describe('Test POST /TEST2', () => {
+describe('Test POST /friends', () => {
   test('Should response with 200 success', () => {
     const response = 200
     expect(response).toBe(200)
--- src/Misc/Express/server.js
+++ src/Misc/Express/server.js
@@ -1,56 +1,6 @@
-import express from 'express'
-
-import { friendsRouter } from './routes/friendsrouter.js'
-import { messagesRouter } from './routes/messagesrouter.js'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-let enableNext = true // Setting this to false, we not get anything back
+import { app } from './app.js'
 
 const PORT = 5000
-const app = express()
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-// Sets req body to be in json format
-app.use(express.json())
-
-// Templating Engine Setup
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'views'))
-
-// Middleware to calculate response time
-app.use((req, res, next) => {
-  const start = Date.now()
-
-  // Notice, if you comment this out. Request times out
-  // Why ? Because, the request cannot go to the next middleware / route handler
-  // This will go to the next route which is (/)
-  if (enableNext) {
-    next()
-  }
-
-  const delta = Date.now() - start
-  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`)
-})
-
-// Express static files from the server
-// This is where you can send html files for websites
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-// Template Engine Example
-app.get('/', (req, res, next) => {
-  // res.send('Hello World !')
-  res.render('index', {
-    title: 'Hello wolrd',
-    caption: 'Quote for the Day',
-  })
-})
-
-// Routers
-app.use('/friends', friendsRouter)
-app.use('/messages', messagesRouter)
 
 // Start Express
 app.listen(PORT, () => {
