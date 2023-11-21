4a78a86f233d0b24f714dbcc67dbd79ebbbbe69d
Tim M <timothy.romero.manas@gmail.com>
2023-11-21 00:04:37 -0500

    Add https and key/certs to securities example

--- src/Misc/Security/server.js
+++ src/Misc/Security/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const https = require('https')
 const path = require('path')
 const express = require('express')
 
@@ -9,6 +11,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(PORT, () => {
-  console.log(`Listening to port: ${PORT}...`)
+app.get('/secret', (req, res) => {
+  res.send(`Secret value is 42`)
 })
+
+// Add certificate and key to server
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening to port: ${PORT}...`)
+  })
