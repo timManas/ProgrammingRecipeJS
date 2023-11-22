bec8f0c770ab6fb9f0e263b235c853663370ef01
Tim M <timothy.romero.manas@gmail.com>
2023-11-21 23:08:59 -0500

    Finish the Google Social Sign on

--- src/Misc/Security/server.js
+++ src/Misc/Security/server.js
@@ -3,21 +3,37 @@ const https = require('https')
 const path = require('path')
 const express = require('express')
 const helmet = require('helmet')
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
+const passport = require('passport')
+const { Strategy } = require('passport-google-oauth20')
 
 const PORT = 3000
 
 // Allow dotenv to fetch environmental variables
-dotenv.config()
+require('dotenv').config()
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 }
 
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google Profile', profile)
+  done(null, profile)
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+
 const app = express()
 
-app.use(helmet())
+app.use(helmet()) // Sets up   helmet which secures our endpoint by encryption
+app.use(passport.initialize()) // Sets up passport which helps authenticate users
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true //TODO
@@ -28,10 +44,25 @@ function checkLoggedIn(req, res, next) {
   next() // Grant access to other API's
 }
 
-app.get('/auth/google', (req, res) => {})
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+)
 
 // This is the callback once google authorizes our credentials
-app.get('/auth/google/callback', (req, res) => {})
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),
+  (req, res) => {
+    console.log('Google called us back')
+  }
+)
 
 // Logout
 app.get('/auth/logout', (req, res) => {})
@@ -39,6 +70,10 @@ app.get('/auth/logout', (req, res) => {})
 // Checks Login Credentials First before allowing access to secret API
 app.get('/secret', checkLoggedIn, (req, res) => {
   res.send(`Secret value is 42`)
+})
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to log in...')
 })
 
 app.get('/', (req, res) => {
