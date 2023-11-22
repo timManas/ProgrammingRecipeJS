093ba17a3b57adaaf8958a4d189d80618b2169f5
Tim M <timothy.romero.manas@gmail.com>
2023-11-21 22:12:51 -0500

    Setup Clientid and secret

--- src/Misc/Security/server.js
+++ src/Misc/Security/server.js
@@ -3,8 +3,17 @@ const https = require('https')
 const path = require('path')
 const express = require('express')
 const helmet = require('helmet')
+const dotenv = require('dotenv')
 
 const PORT = 3000
+
+// Allow dotenv to fetch environmental variables
+dotenv.config()
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
 
 const app = express()
 
