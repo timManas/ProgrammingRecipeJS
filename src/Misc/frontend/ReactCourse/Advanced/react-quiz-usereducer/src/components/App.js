797291f354efc9761e469bbf23ba8340daca0eab
Tim M <timothy.romero.manas@gmail.com>
2023-12-14 22:40:52 -0500

    Adding useReducer example

---
+++ src/Misc/frontend/ReactCourse/Advanced/react-quiz-usereducer/src/components/App.js
@@ -0,0 +1,46 @@
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+import { useQuiz } from "../contexts/QuizContext";
+
+
+/**
+ * How to Run ?
+ * 1. Execute "npm run json-server"
+ * 2. Execute "npm run start"
+ */
+
+export default function App() {
+  const { status } = useQuiz();
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
+        {status === "active" && (
+          <>
+            <Progress />
+            <Question />
+            <Footer>
+              <Timer />
+              <NextButton />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && <FinishScreen />}
+      </Main>
+    </div>
+  );
+}
