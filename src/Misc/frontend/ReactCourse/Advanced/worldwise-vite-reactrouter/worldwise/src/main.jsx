6d98f998717f823cf0b45467ca3ac8db3711f892
Tim M <timothy.romero.manas@gmail.com>
2024-01-13 11:03:20 -0500

    Adding WorldWise using Vite

---
+++ src/Misc/frontend/ReactCourse/Advanced/worldwise-vite-reactrouter/worldwise/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+
+// How to run
+// Run: npm run server
+// Then: npm run dev
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
