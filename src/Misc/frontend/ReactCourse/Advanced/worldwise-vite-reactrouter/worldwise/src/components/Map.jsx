2ad6501bc7d3842bb89d03ea6474edb46878102d
Tim M <timothy.romero.manas@gmail.com>
2024-01-15 08:48:10 -0500

    Adding documentation

--- src/Misc/frontend/ReactCourse/Advanced/worldwise-vite-reactrouter/worldwise/src/components/Map.jsx
+++ src/Misc/frontend/ReactCourse/Advanced/worldwise-vite-reactrouter/worldwise/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 import {
   MapContainer,
   TileLayer,
@@ -6,45 +6,45 @@ import {
   Popup,
   useMap,
   useMapEvents,
-} from "react-leaflet";
+} from 'react-leaflet'
 
-import styles from "./styles/Map.module.css";
-import { useEffect, useState } from "react";
-import { useCities } from "../contexts/CitiesContext";
-import { useGeolocation } from "../hooks/useGeolocation";
-import { useUrlPosition } from "../hooks/useUrlPosition";
-import Button from "./Button";
+import styles from './styles/Map.module.css'
+import { useEffect, useState } from 'react'
+import { useCities } from '../contexts/CitiesContext'
+import { useGeolocation } from '../hooks/useGeolocation'
+import { useUrlPosition } from '../hooks/useUrlPosition'
+import Button from './Button'
 
 function Map() {
-  const { cities } = useCities();
-  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities()
+  const [mapPosition, setMapPosition] = useState([40, 0])
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
-  } = useGeolocation();
-  const [mapLat, mapLng] = useUrlPosition();
+  } = useGeolocation()
+  const [mapLat, mapLng] = useUrlPosition()
 
   useEffect(
     function () {
-      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
     },
     [mapLat, mapLng]
-  );
+  )
 
   useEffect(
     function () {
       if (geolocationPosition)
-        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
     },
     [geolocationPosition]
-  );
+  )
 
   return (
     <div className={styles.mapContainer}>
       {!geolocationPosition && (
-        <Button type="position" onClick={getPosition}>
-          {isLoadingPosition ? "Loading..." : "Use your position"}
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your position'}
         </Button>
       )}
 
@@ -56,7 +56,7 @@ function Map() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
         {cities.map((city) => (
           <Marker
@@ -73,21 +73,23 @@ function Map() {
         <DetectClick />
       </MapContainer>
     </div>
-  );
+  )
 }
 
 function ChangeCenter({ position }) {
-  const map = useMap();
-  map.setView(position);
-  return null;
+  const map = useMap()
+  map.setView(position)
+  return null
 }
 
 function DetectClick() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
+  // This allows us to click on specif places and set the Latitude and Longtitude
+  // How to find the values of lat and lng ? Just console log it then add the variables in here
   useMapEvents({
     click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
-  });
+  })
 }
 
-export default Map;
+export default Map
