e2b9fa949cd67b33ed473211c24bb209b6c5d7c5
Tim M <timothy.romero.manas@gmail.com>
2024-01-21 11:08:38 -0500

    Adding changes to slice

--- src/Misc/frontend/ReactCourse/Advanced/redux-intro/src/features/accounts/accountSlice.js
+++ src/Misc/frontend/ReactCourse/Advanced/redux-intro/src/features/accounts/accountSlice.js
@@ -19,6 +19,8 @@ const accountSlice = createSlice({
       state.balance -= action.payload
     },
     requestLoan: {
+      // Q. WTH is this ?
+      // This is prepare method which has the same parameters as requestLoan(loanAmount, loanPurpose)
       prepare(amount, purpose) {
         return {
           payload: { amount, purpose },
@@ -44,9 +46,13 @@ const accountSlice = createSlice({
   },
 })
 
+//Note: we do not add 'deposit' since it is being export as part of a middleware below
 export const { withdraw, requestLoan, payLoan } = accountSlice.actions
 
+console.log('accountSlice: ' + JSON.stringify(accountSlice))
+
 //Note - This is a middlware function. Hence we dont need to import dispatch
+// Needs to match the name of the action.creator above
 export function deposit(amount, currency) {
   if (currency === 'USD') return { type: 'account/deposit', payload: amount }
 
@@ -66,3 +72,70 @@ export function deposit(amount, currency) {
 }
 
 export default accountSlice.reducer
+
+/*
+export default function accountReducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      // LATER
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
+
+    default:
+      return state;
+  }
+}
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+export function payLoan() {
+  return { type: "account/payLoan" };
+}
+*/
--- src/Misc/frontend/ReactCourse/Advanced/redux-intro/src/features/customers/customerSlice.js
+++ src/Misc/frontend/ReactCourse/Advanced/redux-intro/src/features/customers/customerSlice.js
@@ -11,6 +11,10 @@ const customerSlice = createSlice({
   initialState,
   reducers: {
     createCustomer: {
+      // prepare the data
+      // Why do we do this ? Because reducers should be vanilla and NOT contain
+      // Any async calls, timers, converts, etc
+      // it should simply update the state+
       prepare(fullName, nationalID) {
         return {
           payload: {
@@ -32,6 +36,40 @@ const customerSlice = createSlice({
   },
 })
 
+console.log('customerSlice: ' + JSON.stringify(customerSlice))
 export const { createCustomer, updateName } = customerSlice.actions
 
 export default customerSlice.reducer
+
+// This was the old way !!!!!!
+// Notice we had no reducer, createCustomer and updateName in the new version
+// This is now possible due to RTK !!!
+
+/* 
+export default function customerReducer(state = initialState, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+*/
